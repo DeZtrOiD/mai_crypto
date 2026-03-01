68807a7acabb999476cd35539bc8da044c28bff7
DeZtrOiD <65456989+DeZtrOiD@users.noreply.github.com>
2026-03-02 00:57:34 +0300

    ff

--- RSA/wiener_attack.cs
+++ RSA/wiener_attack.cs
@@ -11,8 +11,9 @@ public static class WienerAttack {
             throw new ArgumentException("n and e must be positive.");
 
         var (numerator, denominator) = (e, n);
-        var convergentH = (BigInteger.Zero, BigInteger.One);
-        var convergentK = (BigInteger.One, BigInteger.Zero);
+        // convergent
+        var conP = (BigInteger.Zero, BigInteger.One); // p 0 1
+        var conQ = (BigInteger.One, BigInteger.Zero); // q 1 0
 
         var convergents = new List<(BigInteger, BigInteger)>();
 
@@ -20,18 +21,18 @@ public static class WienerAttack {
             BigInteger quotient = numerator / denominator;
             (numerator, denominator) = (denominator, numerator % denominator);
 
-            BigInteger newH = quotient * convergentH.Item2 + convergentH.Item1;
-            BigInteger newK = quotient * convergentK.Item2 + convergentK.Item1;
+            BigInteger newP = quotient * conP.Item2 + conP.Item1;
+            BigInteger newQ = quotient * conQ.Item2 + conQ.Item1;
 
-            (convergentH.Item1, convergentH.Item2) = (convergentH.Item2, newH);
-            (convergentK.Item1, convergentK.Item2) = (convergentK.Item2, newK);
+            (conP.Item1, conP.Item2) = (conP.Item2, newP);
+            (conQ.Item1, conQ.Item2) = (conQ.Item2, newQ);
             // add p, q
-            convergents.Add((convergentH.Item2, convergentK.Item2));
+            convergents.Add((conP.Item2, conQ.Item2));
 
-            if (convergentK.Item2 > 1 && convergentH.Item2 > 1) {
-                BigInteger phiNumerator = e * convergentK.Item2 - 1;
-                if (phiNumerator % convergentH.Item2 == 0) {
-                    BigInteger phi = phiNumerator / convergentH.Item2;
+            if (conQ.Item2 > 1 && conP.Item2 > 1) {
+                BigInteger phiNumerator = e * conQ.Item2 - 1;
+                if (phiNumerator % conP.Item2 == 0) {
+                    BigInteger phi = phiNumerator / conP.Item2;
 
                     BigInteger sumPQ = n - phi + 1;
                     BigInteger discriminant = sumPQ * sumPQ - 4 * n;
@@ -41,7 +42,7 @@ public static class WienerAttack {
                         if (sqrtDisc * sqrtDisc == discriminant) {
                             BigInteger p = (sumPQ + sqrtDisc) / 2;
                             if (p > 1 && n % p == 0) {
-                                return new WienerAttackResult(convergentK.Item2, phi, convergents);
+                                return new WienerAttackResult(conQ.Item2, phi, convergents);
                             }
                         }
                     }
